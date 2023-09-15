02ebe547a98c400794734ee801b86610565ebbed
PaulxT0 <Paulalexisbauer04@gmail.com>
2023-09-15 11:24:12 -0400

    Final tweaks
    Final tweaks

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let scrollSecond = document.querySelector(".scroll-second");
 let scrollThird = document.querySelector(".scroll-third");
 let container = document.querySelector(".container");
 
-// elements needed for scroll button functionality
 let downArrow = document.querySelector(".down-btn"); 
 let upArrow = document.querySelector(".up-btn");
 let featuresItems = document.querySelector(".features-items")
@@ -12,44 +11,39 @@ let homeBtn = document.querySelector(".home-btn");
 
 homeBtn.addEventListener("click", e => {
     scrollFirst.scrollIntoView();
+    currentIndex = 0;
 })
 
 let scrollContainer = document.querySelector(".scroll-container");
 
-let scrollPageArray = [ // holds references for scrolling
+let scrollPageArray = [
     scrollFirst,
     scrollSecond,
     scrollThird
 ];
-// offsetTop returns height diff between scroll elm and parent elm
 
 let currentIndex = 0;
 
-// window.addEventListener("scrollend",e => {
-//     currentIndex = // either scrollFirst,scrollSecond, or ScrollThird, depending on which is closest
-// })
-
 window.addEventListener("DOMContentLoaded", e => {
-    window.scrollTo({
+    scrollContainer.scrollTo({
         top: scrollPageArray[currentIndex]
     }); 
+    let containerArr = document.querySelectorAll(".scroll-child");
+    let tempHeight = container.getBoundingClientRect().height;
+    containerArr.forEach(element => {
+        element.style.height = tempHeight - headerElm.getBoundingClientRect().height;
+        console.log(element.getBoundingClientRect().height);
+    });
 });
 
 downArrow.addEventListener("click", e => {
-    scrollThroughElms(false);
-});
-upArrow.addEventListener("click", e => {
-    scrollThroughElms(true);
-});
-
-function scrollThroughElms(scrollingUp) {
-    if (scrollingUp) {
-        currentIndex = currentIndex - 1 >= 0 ? currentIndex -1 : 2
-    } else {
-        currentIndex = currentIndex + 1 <= 2 ? currentIndex +1 : 0
-    }
-
+    currentIndex = (currentIndex + 1 <= 2) ? currentIndex + 1 : 0;
     scrollPageArray[currentIndex].scrollIntoView();
-}
+});
 
-// ADD SOME WAY TO DETECT SCROLL AND UPDATE CURRENTINDEX BASED ON POSITION
+upArrow.addEventListener("click", e => {
+    currentIndex = (currentIndex - 1 >= 0) ? currentIndex - 1 : 2;
+    scrollPageArray[currentIndex].scrollIntoView();
+});
+
+
